d011a628e7c96a51fb8df6c0ddc22fe40a957df4
bismuthnickel <warreneverettstevens@gmail.com>
2025-04-15 20:27:49 -0400

    change for consistency

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -12,11 +12,11 @@ void kernel(void)
     format_tty(0x07);
 
     while ((key = wait_key()) != 0x01) {
-        if (convert_scancode_to_ascii(key) == 0x0a)
+        if (convert_scancode_to_ascii(key) == 0x0a) {
             puts("\r\n");
-        else if (convert_scancode_to_ascii(key) == 0x08)
+        } else if (convert_scancode_to_ascii(key) == 0x08) {
             puts("\b \b");
-        else if (convert_scancode_to_ascii(key) == '=') {
+        } else if (convert_scancode_to_ascii(key) == '=') {
             textcursor_t* cursor = get_cursor();
             if (cursor->row > 0)
                 set_cursor(cursor->row - 1, cursor->col);
@@ -32,8 +32,9 @@ void kernel(void)
             textcursor_t* cursor = get_cursor();
             if (cursor->row < 24)
                 set_cursor(cursor->row + 1, cursor->col);
-        } else
+        } else {
             putc(convert_scancode_to_ascii(key));
+        }
     }
 
     putsf("\r\nSalutations, Planet in which I reside on!\r\n", 0x0b);
