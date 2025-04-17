1cf8f90c314677b58d315f28494699809bba2777
bismuth <70106649+bismuthnickel@users.noreply.github.com>
2025-04-16 21:21:58 -0400

    idk

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -25,20 +25,16 @@ void kernel(void)
             puts("\b \b");
         } else if (convert_scancode_to_ascii(key) == '=') {
             textcursor_t* cursor = get_cursor();
-            if (cursor->row > 0)
-                set_cursor(cursor->row - 1, cursor->col);
+            if (cursor->row > 0) set_cursor(cursor->row - 1, cursor->col);
         } else if (convert_scancode_to_ascii(key) == '[') {
             textcursor_t* cursor = get_cursor();
-            if (cursor->col > 0)
-                set_cursor(cursor->row, cursor->col - 1);
+            if (cursor->col > 0) set_cursor(cursor->row, cursor->col - 1);
         } else if (convert_scancode_to_ascii(key) == ']') {
             textcursor_t* cursor = get_cursor();
-            if (cursor->col < 79)
-                set_cursor(cursor->row, cursor->col + 1);
+            if (cursor->col < 79) set_cursor(cursor->row, cursor->col + 1);
         } else if (convert_scancode_to_ascii(key) == '\'') {
             textcursor_t* cursor = get_cursor();
-            if (cursor->row < 24)
-                set_cursor(cursor->row + 1, cursor->col);
+            if (cursor->row < 24) set_cursor(cursor->row + 1, cursor->col);
         } else {
             putc(convert_scancode_to_ascii(key));
         }
